89e07bf33315fa4f71075f3e239f26aa4b9fab2d
Jahanvi Thakkar <82188673+JahanviT@users.noreply.github.com>
2022-09-14 01:44:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+prompt("Enter your name");
+prompt("Enter the name of the person you love");
+var n = Math.random() * 100;
+n = Math.floor(n) + 1;
+if (n > 80){
+alert("Your love score is " + n + "%. " + "WOAH!! You love each other like Kanye loves Kanye.");
+} else {
+alert("Your love score is " + n + "%" );
+}
